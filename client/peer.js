e02e37ffeab2f14d10477e9b926052a21cce2bb4
alexfigliolia <alexfigliolia@gmail.com>
2017-12-04 01:13:51 -0500

    update

--- client/peer.js
+++ client/peer.js
@@ -9,7 +9,11 @@ const Peer = {
 	accepted: null,
 
 	init: (user) => {
-		Peer.socket = io('https://piper-signaler.herokuapp.com/', {transports: ['websocket']});
+		// Peer.socket = io('https://piper-signaler.herokuapp.com/', {transports: ['websocket']});
+    Peer.socket = io(window.location.protocol + '//piper-signaler.herokuapp.com', {
+      'multiplex': false,
+      'path': '/'
+    });
 		Peer.socket.emit('connected', user);
 		Peer.socket.on('offer', Peer.onOffer);
 		Peer.socket.on('uniqueID', (uniqueID) => {
